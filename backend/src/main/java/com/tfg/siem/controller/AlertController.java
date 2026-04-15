15a9f49674c4f8b219ddb15233a6da64351043fc
quico16 <quico1607@gmail.com>
2026-04-15 17:06:44 +0200

    fix: AlertController import and make backend tests independent from local postgres

--- backend/src/main/java/com/tfg/siem/controller/AlertController.java
+++ backend/src/main/java/com/tfg/siem/controller/AlertController.java
@@ -6,6 +6,7 @@ import com.tfg.siem.service.AlertService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
